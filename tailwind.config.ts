d90b146a166e3cd18d7a9a77b3c8c64254210668
Emmanuel Bayona <Bayonaj368@gmail.com>
2023-10-08 14:51:31 -0600

    add title

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'radial-dark': 'radial-gradient(66.48% 66.48% at 50% -0.7%, #222 0%, #111 100%), url(/noise.png)',
+			},
+			dropShadow: {
+				'white-neon': '0px 0px 10px rgba(255, 255, 255, 0.40)',
+			}
+		},
+	},
+	plugins: [],
 }
 export default config
