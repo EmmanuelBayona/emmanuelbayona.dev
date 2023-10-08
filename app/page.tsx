1aa35201a743b0e7a7ea74cb162c36bb0715eacb
Emmanuel Bayona <Bayonaj368@gmail.com>
2023-10-08 14:55:27 -0600

    responsive title

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<>
 			<section className="max-w-4xl mx-auto mt-12 text-center">
-				<h1 className="text-8xl font-bold text-white drop-shadow-white-neon">
+				<h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-white drop-shadow-white-neon">
 					I create and bring <br /> products to <span className="italic">life</span>.
 				</h1>
 			</section>
