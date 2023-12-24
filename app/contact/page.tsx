0db85e2fb8a2007a4d8020ff9706e52e2f63e51a
Emmanuel <bayonaj368@gmail.com>
2023-12-24 01:52:13 -0600

    fix: main page initial scroll

---
+++ app/contact/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Contact',
+    description: 'Contact form for Emmanuel Bayona\'s portfolio',
+}
+
+export default function Contact() {
+
+    return (
+        <>
+        
+        </>
+    )
+
+}
