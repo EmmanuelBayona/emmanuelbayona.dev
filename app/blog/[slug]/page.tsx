463e795cd50499aec3f7819484060a1c53e2273d
Emmanuel Bayona <bayonaj368@gmail.com>
2024-03-20 14:56:26 -0600

    fix metadata type error

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next"
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { getBlogPosts, getBlogTitles } from "lib/blogs";
 import { MaxWidthWrapper } from "components/max-width-wrapper";
@@ -6,9 +6,11 @@ import { CustomMDX } from "components/mdx";
 import { formatDateUS } from "lib/utils";
 import { BlogTitles } from "components/blog-titles";
 
-
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata | undefined> {
-
+export async function generateMetadata({
+    params,
+}: {
+    params: { slug: string };
+}): Promise<Metadata | undefined> {
     const post = getBlogPosts().find((post) => post.slug === params.slug);
     if (!post) return;
 
@@ -16,12 +18,12 @@ export async function generateMetadata({ params }: { params: { slug: string } })
         title,
         publishedAt: publishedTime,
         summary: description,
-        image
+        image,
     } = post.metadata;
 
     const ogImage = image
         ? new URL(`https://emmanuelbayona.dev${image}`)
-        : new URL(`https://emmanuelbayona.dev/cubes-algorithms.png`)
+        : new URL(`https://emmanuelbayona.dev/cubes-algorithms.png`);
 
     return {
         metadataBase: new URL(`https://emmanuelbayona.dev/blog/${post.slug}`),
@@ -30,23 +32,21 @@ export async function generateMetadata({ params }: { params: { slug: string } })
         openGraph: {
             title,
             description,
-            type: 'article',
+            type: "article",
             publishedTime,
             url: `https://emmanuelbayona.dev/blog/${post.slug}`,
             images: [{ url: ogImage }],
-            twitter: {
-                card: 'summary_large_image',
-                title,
-                description,
-                images: [ogImage],
-            }
         },
-    }
-
+        twitter: {
+            card: "summary_large_image",
+            title,
+            description,
+            images: [ogImage],
+        },
+    };
 }
 
 export default function Blog({ params }: { params: { slug: string } }) {
-
     const post = getBlogPosts().find((post) => post.slug === params.slug);
 
     if (!post) notFound();
@@ -55,11 +55,8 @@ export default function Blog({ params }: { params: { slug: string } }) {
 
     return (
         <MaxWidthWrapper className="mt-5 lg:mt-14 lg:flex lg:flex-col lg:items-center relative">
-
             <div className="absolute top-0 right-5 hidden xl:block h-full">
-                <BlogTitles titles={blogTitles}
-                    className="sticky top-5"
-                />
+                <BlogTitles titles={blogTitles} className="sticky top-5" />
             </div>
 
             <section>
@@ -68,8 +65,8 @@ export default function Blog({ params }: { params: { slug: string } }) {
                     suppressHydrationWarning
                     dangerouslySetInnerHTML={{
                         __html: JSON.stringify({
-                            '@context': 'https://schema.org',
-                            '@type': 'BlogPosting',
+                            "@context": "https://schema.org",
+                            "@type": "BlogPosting",
                             headline: post.metadata.title,
                             datePublished: post.metadata.publishedAt,
                             dateModified: post.metadata.publishedAt,
@@ -77,10 +74,10 @@ export default function Blog({ params }: { params: { slug: string } }) {
                             image: post.metadata.image,
                             url: `https://emmanuelbayona.dev/blog/${post.slug}`,
                             author: {
-                                '@type': 'Person',
-                                name: 'Emmanuel Bayona',
-                            }
-                        })
+                                "@type": "Person",
+                                name: "Emmanuel Bayona",
+                            },
+                        }),
                     }}
                 />
                 <h1 className="font-medium text-2xl lg:text-3xl tracking-tighter max-w-[650px] text-white">
@@ -94,9 +91,7 @@ export default function Blog({ params }: { params: { slug: string } }) {
                 <article className="prose prose-neutral text-zinc-400 pb-5 lg:pb-10">
                     <CustomMDX source={post.content} />
                 </article>
-
             </section>
         </MaxWidthWrapper>
-    )
-
+    );
 }
