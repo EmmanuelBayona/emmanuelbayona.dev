bfa7ce91fe18a2ee6ef4740969bed0be7d049e4a
Emmanuel <bayonaj368@gmail.com>
2024-03-21 00:58:22 -0600

    add new background and text colors

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@ import { MaxWidthWrapper } from "components/max-width-wrapper";
 import { ProjectsGrid } from "components/project-grid";
 
 export default function Home() {
-	return (
-		<MaxWidthWrapper className="mt-12 lg:mt-28">
-			<section className="max-w-[650px] mx-auto text-center">
-				<h1 className="text-3xl md:text-4xl lg:text-6xl font-bold text-white drop-shadow-white-neon">
-					hey, I&apos;m Emmanuel 👋
-				</h1>
-				<p className="mt-8 text-base md:text-xl lg:text-2xl text-zinc-300 max-w-[6500px]">
-					Frontend Developer creating clean and intuitive user interfaces. Let&apos;s build something beautiful together.
-				</p>
-			</section>
+    return (
+        <MaxWidthWrapper className="mt-12 lg:mt-28">
+            <section className="max-w-[650px] mx-auto text-center">
+                <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold text-white">
+                    Hey, I&apos;m Emmanuel 🧩
+                </h1>
+                <p className="mt-8 text-base md:text-xl lg:text-2xl text-zinc-400 max-w-[6500px]">
+                    Frontend Developer creating clean and intuitive user interfaces. Let&apos;s build something beautiful together.
+                </p>
+            </section>
 
-			<ProjectsGrid />
-		</MaxWidthWrapper>
-	)
+            <ProjectsGrid />
+        </MaxWidthWrapper>
+    )
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -3,13 +3,21 @@ import Link from "next/link"
 export const Navbar = () => {
 
     return (
-        <nav className="w-full h-14 border-b border-dark-accent flex justify-between items-center px-6 md:px-10 bg-transparent">
+        <nav className="bg-zinc-950 text-sm text-zinc-300 shadow-[inset_0_-1px_0_0_rgb(39,39,42)] h-12 flex justify-between items-center px-6 md:px-10 bg-transparent">
 
-            <Link className="text-white text-sm"
-                href='/'
-            >
+            <Link href='/' className="hover:text-zinc-400">
                 Home
             </Link>
+
+            <div className="flex gap-4">
+                <Link href='/' className="hover:text-zinc-400">
+                    Blog
+                </Link>
+
+                <Link href='/' className="hover:text-zinc-400">
+                    Projects
+                </Link>
+            </div>
 
         </nav>
     )
